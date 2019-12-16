3de06f287c296fbd2fa5ffd273cc477a721cf266
Тимофей Забалуев <zabti@yandex.ru>
2019-12-16 20:47:25 +0400

    Initial Commit

---
+++ RedmadrobotTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedmadrobotTestProject
+//
+//  Created by Тимофей Забалуев on 16.12.2019.
+//  Copyright © 2019 Тимофей Забалуев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
